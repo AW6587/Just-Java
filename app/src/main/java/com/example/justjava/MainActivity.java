c881ce42ae4e22da4eb036587f299afe50b802d7
Midori-Williams <midoriww@uw.edu>
2020-03-09 23:27:58 -0700

    changed comment on increment method and removed redundent TextView casting

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         if (numberOfCoffees > 0){
             numberOfCoffees -= 1;
             display();
-        } // else do nothing
+        } // else do nothing, does not need to update
     }
 
     /**
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display() {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + numberOfCoffees);
     }
 
@@ -60,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice() {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(numberOfCoffees * cost));
+
     }
 }
